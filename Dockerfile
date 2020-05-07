ed4d5bdcab1fdbd78d65516dd2e65a8757604aed
Marco Legramandi <marco.legramandi@sparkfabrik.com>
2020-05-07 11:19:43 +0200

    refs #48: add new sparktheme design

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,25 @@
 FROM sparkfabrik/docker-locke-server:latest
-MAINTAINER Paolo Pustorino <paolo.pustorino@sparkfabrik.com>
+LABEL author="Paolo Pustorino <paolo.pustorino@sparkfabrik.com>"
 
 # Remove content folder
 RUN rm -rf content/
 
 # Copy content and configuration
 COPY ./custom/config.js /srv/locke/config.js
-COPY ./custom/custom-styles.css /srv/locke/themes/spark/public/styles/custom.css
+COPY ./custom/themes /srv/locke/themes
 COPY ./content /srv/locke/content
 COPY ./assets /srv/locke/assets
+
+# This file copy is a simple and effective way
+# to enchance RANETO waiting for the pull request
+# in the project to be accepted.
+COPY ./custom/raneto/contents.js /srv/locke/node_modules/raneto/app/core/contents.js
+
+WORKDIR /srv/locke/themes/spark-playbook
+
+RUN \
+  npm update && \
+  npm install && \
+  npm run build
+
+WORKDIR /srv/locke/
---
+++ custom/themes/spark-playbook/webpack.config.js
@@ -0,0 +1,107 @@
+const path = require('path');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = env => {
+  // inProduction is true when we are building the production code.
+  const inProduction = env.NODE_ENV === 'production';
+  const sourceMapEnabled = !!env.enableSourceMap;
+
+  return {
+    plugins: [
+      // Process SCSS input files before converting to CSS,
+      // using Stylelint specific config files (see theme root folder).
+      new StylelintPlugin({
+        // fix: inProduction, // Enable if you want to autofix SCSS files when building production dist files.
+        lintDirtyModulesOnly: true,
+      })
+    ],
+
+    entry: [
+      './public/js/app.js',
+      './scss/app.scss'
+    ],
+
+    output: {
+      filename: 'dist/main.js',
+      path: path.resolve(__dirname, 'public'),
+      // publicPath: '../',
+    },
+
+    watch: false,
+
+    optimization: {
+      minimizer: [new UglifyJsPlugin()],
+    },
+
+    module: {
+      rules: [
+        // Javascript files.
+        {
+          test: /\.js?$/,
+          exclude: /(node_modules|vendor)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          },
+        },
+        {
+          test: /\.scss$/,
+          exclude: /(node_modules|vendor)/,
+          use: [
+            // Save the output CSS file.
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'dist/style.css',
+              }
+            },
+            // Extract the CSS from the bundle.
+            {
+              loader: "extract-loader",
+            },
+            // Translates CSS into CommonJS.
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: sourceMapEnabled,
+                url: false,
+                import: true,
+                importLoaders: 2, // 2 => postcss-loader, sass-loader
+              }
+            },
+            // Loader for webpack to process CSS with PostCSS
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                sourceMap: sourceMapEnabled,
+                plugins: [
+                  require('postcss-import')(),
+                  // Autoprefixer get settings from the .browserslistrc file
+                  require('autoprefixer'),
+                ],
+                config: {
+                  ctx: {
+                    cssnano: {
+                      preset: 'default',
+                    }
+                  }
+                }
+              }
+            },
+            // Compiles Sass to CSS.
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: sourceMapEnabled,
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
