e5a6f535bbc9cb52c1cb85bd3ea68c0392024949
Marcello Gorla <marcello.gorla@gmail.com>
2023-10-19 17:58:06 +0200

    refs #199: Mermaid support (#202)
    * refs #199: Added basic support for mermaid with view source functionality
    * refs #199: Styling and JS are slightly more readable now

--- custom/themes/spark-playbook/public/js/app.js
+++ custom/themes/spark-playbook/public/js/app.js
@@ -44,20 +44,38 @@ $(() => {
 
   function shoutCopied(el) {
     var copyShout = document.createElement('div');
-    copyShout.classList.add('code-copy-shout');
+    copyShout.classList.add('code-ui--shout', 'code-ui');
     copyShout.innerHTML = 'Copied!';
     const containers = el.parentElement.getElementsByClassName('code-copy-button-container')
     if (containers.length < 0) {
       return;
     }
     containers[0].appendChild(copyShout);
-    copyShout.classList.add('code-copy-shout--show');
+    copyShout.classList.add('code-ui--shout__show');
     setTimeout(() => {
-      copyShout.classList.remove('code-copy-shout--show');
+      copyShout.classList.remove('code-ui--shout__show');
     }, 1000)
     setTimeout(() => {
       copyShout.remove();
     }, 2000)
+  }
+
+  function booleanToogle(button) {
+    button.classList.toggle('on');
+  }
+
+  function showDiagramCode(diagram) {
+    const source = diagram.nextSibling;
+    if (source && diagram) {
+      diagram.classList.toggle('hide');
+      source.classList.toggle('hide');
+    }
+  }
+
+  function createButton(classes) {
+    const button = document.createElement('div');
+    button.classList.add(...classes);
+    return button;
   }
 
   // Syntax highlighting
@@ -75,12 +93,11 @@ $(() => {
     })
   }
 
-  const code = $("pre code")
+  const code = $("pre code");
   if (code) {
     code.each((i, el) => {
       hljs.highlightElement(el);
-      var button = document.createElement('div');
-      button.classList.add('code-copy-button');
+      var button = createButton(['code-ui', 'code-ui--button', 'code-ui--button--copy']);
       var buttonContainer = document.createElement('div');
       buttonContainer.classList.add('code-copy-button-container');
       buttonContainer.append(button);
@@ -91,6 +108,21 @@ $(() => {
         shoutCopied(el);
       });
     });
+
+    const mermaid = $("pre.mermaid");
+    if (mermaid) {
+      mermaid.each((i, diagram) => {
+        var button = createButton(['code-ui', 'code-ui--button', 'code-ui--button--switch']);
+        var buttonContainer = document.createElement('div');
+        buttonContainer.classList.add('mermaid-switch-button-container');
+        buttonContainer.append(button);
+        diagram.before(buttonContainer);
+        button.addEventListener('click', (e) => {
+          booleanToogle(button);
+          showDiagramCode(diagram);
+        });
+      });
+    }
   }
 
   // Enable Highlighting and other
