c277440cc3c36983e78ffed7d78677b45edba449
Alessio Cimarelli <alessio.cimarelli@sparkfabrik.com>
2024-07-06 17:35:35 +0200

    feat: add cors to api
    Co-authored-by: Paolo Pustorino <stickgrinder@users.noreply.github.com>
    Co-authored-by: Osvaldo Tulini <osvaldo.tulini@gmail.com>

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
+import cors from '@fastify/cors';
 import { answer, sources } from "./lib/qna";
-import { loadDocuments } from "./lib/loader";
+// import { loadDocuments } from "./lib/loader";
 
 // Create Fastify server instance
 const server = fastify();
@@ -26,23 +27,26 @@ server.get<{ Querystring: { question: string } }>('/ask', { schema: getSchema },
   reply.send(response);
 });
 
-// PUT webhook to trigger documents reloading
-server.put('/api/load-documents', async (request, reply) => {
-    try {
-      await loadDocuments();
-      reply.send({ status: 'success', message: 'Documents loaded successfully' });
-    } catch (error) {
-      console.error(error);
-      reply.status(500).send({ status: 'error', message: 'Failed to load documents' });
-    }
-  });
+// // PUT webhook to trigger documents reloading
+// server.put('/api/load-documents', async (request, reply) => {
+//     try {
+//       await loadDocuments();
+//       reply.send({ status: 'success', message: 'Documents loaded successfully' });
+//     } catch (error) {
+//       console.error(error);
+//       reply.status(500).send({ status: 'error', message: 'Failed to load documents' });
+//     }
+//   });
 
   
 // Start server
 const start = async () => {
+  await server.register(cors, { 
+    origin: '*'
+  });
   try {
-    await server.listen({ port: 80 });
-    console.log('Server listening on http://localhost');
+    await server.listen({ port: 3000 });
+    console.log('Server listening on http://localhost:3000');
   } catch (err) {
     console.error(err);
     process.exit(1);
