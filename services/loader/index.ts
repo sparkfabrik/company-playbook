a2b9e34026a13cb349abf3e18b330f76d76a4f90
Alessio Cimarelli <alessio.cimarelli@sparkfabrik.com>
2024-07-06 16:32:34 +0200

    feat: api service in container
    Co-authored-by: Paolo Pustorino <stickgrinder@users.noreply.github.com>
    Co-authored-by: Osvaldo Tulini <osvaldo.tulini@gmail.com>

--- services/loader/index.ts
+++ services/loader/index.ts
@@ -1,26 +1,4 @@
-import "dotenv/config";
+import { loadDocuments } from "../lib/loader.ts";
 
-import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
-import { TextLoader } from "langchain/document_loaders/fs/text";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+loadDocuments();
 
-import { createStore } from "../lib/chroma.js";
-
-const COLLECTION = "rag-playbook";
-
-export const getDocs = async () => {
-  const loader = new DirectoryLoader(`../content`, {
-    ".md": (path) => new TextLoader(path),
-  });
-  const docs = await loader.load();
-
-  // Split it chunks.
-  const splitter = RecursiveCharacterTextSplitter.fromLanguage("markdown", {
-    chunkSize: 1500,
-    chunkOverlap: 0,
-  });
-  const split = await splitter.splitDocuments(docs);
-  return split;
-};
-
-createStore(COLLECTION, getDocs);
